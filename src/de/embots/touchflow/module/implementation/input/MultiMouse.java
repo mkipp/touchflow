bcfb24b21fc437008d4d1fc8c88b5787491197e4
frederic.raber <frederic.raber@gmail.com>
2011-11-22 19:05:02 +0100

    FIXME

--- src/de/embots/touchflow/module/implementation/input/MultiMouse.java
+++ src/de/embots/touchflow/module/implementation/input/MultiMouse.java
@@ -11,30 +11,25 @@ import org.jdom.Element;
 import de.embots.touchflow.exceptions.ModulException;
 import de.embots.touchflow.module.core.InputModule;
 import de.embots.touchflow.module.core.PinName;
+import de.embots.touchflow.module.implementation.input.MMouse.MouseBuffer;
+import de.embots.touchflow.module.implementation.input.MMouse.MouseState;
 import de.embots.touchflow.module.pin.OutputPin;
 import de.embots.touchflow.module.pin.OutputPin2D;
 
-public class MultiMouse  extends InputModule implements Runnable{
+public class MultiMouse  extends InputModule{
 
-	int port=1235;
-	
-	static int xpos=0;
-	static int ypos=0;
-	
-	private static ServerSocket server;
-	boolean running=false;
-	static boolean isLMouseDown=false;
-	static boolean isRMouseDown=false;
-	private static BufferedReader in;
-	private static Thread thisThread=null;
-	
+	static MouseBuffer server;
+	int id=1;
+
 	
 	@Override
 	protected void processData() throws ModulException {
 		OutputPin2D out=getOutputPin2D(PinName.POSITION);
-		out.writeData(xpos);
-		out.writeData2(ypos);
-	
+		MouseState state=server.getMouseState(id);
+		if (state!=null){
+			out.writeData(state.getXpos());
+			out.writeData2(state.getYpos());
+		}
 	}
 
 	@Override
@@ -52,117 +47,12 @@ public class MultiMouse  extends InputModule implements Runnable{
 	public MultiMouse(){
 		outputPins=new OutputPin[1];
 		outputPins[0]=new OutputPin2D(PinName.POSITION,this);
-		startListenerThread();
-	}
-
-	@Override
-	public void run() {
-		try {
-			startServer();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		while(running){	
-			String msg="";
-			try {
-				msg = in.readLine();
-				parseMSG(msg);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		}
-		try {
-			server.close();
-			in.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-		
-		
-	}
-
-	private void parseMSG(String msg) {
-		msg.trim();
-		String[] parts=msg.split(" ");
-		if (parts.length!=3){
-			System.err.println("WARNING: MultiMouse:could not parse/split msg:" + msg);
-			return;
-		}
-		
-		if (parts[0].equals("motion")){
-			String[] xcor=parts[1].split("=");
-			String[] ycor=parts[2].split("=");
-			
-			if (xcor.length!=2 || ycor.length!=2){
-				System.err.println("WARNING: MultiMouse:could not parse/2split msg:" + msg);
-				return;
-			}
-			
-			try{
-				xpos=Integer.parseInt(xcor[1]);
-				ypos=Integer.parseInt(ycor[1]);
-			}
-			catch(NumberFormatException nf){
-				System.err.println("WARNING: MultiMouse:could not parse/numberParse msg:" + msg);
-			}
-			return;
-		}
-		
-		if (parts[0].equals("button")){
-			if (parts[1].equals("press")){
-				if (parts[2].equals("1")){
-					this.isLMouseDown=true;
-				}
-				else if(parts[2].equals("2")){
-					this.isRMouseDown=true;
-				}
-			}
-			else{
-				if (parts[2].equals("1")){
-					this.isLMouseDown=false;
-				}
-				else if(parts[2].equals("2")){
-					this.isRMouseDown=false;
-				}
-			}
-			return;
-		}
-		
-		System.err.println("WARNING: MultiMouse:could not parse/get type of msg:" + msg);
-		
-	}
-
-	@Override
-	public void stopRunning() {
-		// TODO Auto-generated method stub
-		super.stopRunning();
-		running=false;
-	}
-
-	public void startServer() throws IOException{
 		if (server==null){
-			System.out.println("Try MM server on port " + port);
-			server=new ServerSocket(port);
-			System.out.println("Created MM server on port " + port);
-			Socket s=server.accept();
-			
-			in = new BufferedReader(new InputStreamReader(
-	                   s.getInputStream()));
+			server=new MouseBuffer();
 			
 		}
+	}
 
-		
-	}
-	public void startListenerThread(){
-		if (thisThread==null){
-			Thread t=new Thread(this);
-			running=true;
-			t.start();
-		}
-	}
+
+	
 }
---
+++ tools/mouseSend2.sh
@@ -0,0 +1,1 @@
+stdbuf -i0 -o0 -e0 xinput test 9|stdbuf -i0 -o0 -e0 ./strApp.sh m2|telnet localhost 1235
